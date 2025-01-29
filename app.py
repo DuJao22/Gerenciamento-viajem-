2c1d9605a1c16cbfdad29e35068f2691b03f0cec
João Layon <104402531+DuJao22@users.noreply.github.com>
2025-01-29 12:12:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
-import psycopg2
-from psycopg2 import sql
+import sqlite3
 from datetime import datetime
 import os
 
 app = Flask(__name__)
 app.secret_key = 'super_secret_key'
 
-DB_HOST = "dpg-cud3bjd6l47c7385qecg-a"
-DB_USER = "database_shki_user"
-DB_PASSWORD = "m0QsRSe6eUWAj70RuxAUJzmcj2dymZoO"
-DB_NAME = "database_shki"
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-
-def get_db_connection(dbname=DB_NAME):
-    conn = psycopg2.connect(
-        dbname=dbname, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
-    )
-    return conn
-
-def create_database():
-    conn = psycopg2.connect(
-        dbname="postgres", user=DB_USER, password=DB_PASSWORD, host=DB_HOST
-    )
-    conn.autocommit = True
-    cursor = conn.cursor()
-
-    cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(DB_NAME)))
-
-    cursor.close()
-    conn.close()
-
 def init_db():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
+    conn = sqlite3.connect('database.db')
+    conn.execute('''
         CREATE TABLE IF NOT EXISTS usuarios (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             nome TEXT,
             idade INTEGER,
             data_nascimento TEXT,
@@ -44,9 +18,9 @@ def init_db():
             senha TEXT
         )
     ''')
-    cursor.execute('''
+    conn.execute('''
         CREATE TABLE IF NOT EXISTS viagens (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             usuario_id INTEGER,
             numero_nota TEXT,
             destino TEXT,
@@ -55,9 +29,9 @@ def init_db():
             FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
         )
     ''')
-    cursor.execute('''
+    conn.execute('''
         CREATE TABLE IF NOT EXISTS gastos (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY,
             viagem_id INTEGER,
             motivo TEXT,
             observacao TEXT,
@@ -65,20 +39,13 @@ def init_db():
             FOREIGN KEY(viagem_id) REFERENCES viagens(id)
         )
     ''')
-    conn.commit()
-    cursor.close()
     conn.close()
 
 def generate_report(viagem_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM viagens WHERE id = %s', (viagem_id,))
-    viagem = cursor.fetchone()
-    cursor.execute('SELECT * FROM gastos WHERE viagem_id = %s', (viagem_id,))
-    gastos = cursor.fetchall()
-    cursor.execute('SELECT * FROM usuarios WHERE id = %s', (viagem[1],))
-    user = cursor.fetchone()
-    cursor.close()
+    conn = sqlite3.connect('database.db')
+    viagem = conn.execute('SELECT * FROM viagens WHERE id = ?', (viagem_id,)).fetchone()
+    gastos = conn.execute('SELECT * FROM gastos WHERE viagem_id = ?', (viagem_id,)).fetchall()
+    user = conn.execute('SELECT * FROM usuarios WHERE id = ?', (viagem[1],)).fetchone()
     conn.close()
 
     total_gastos = sum(gasto[4] for gasto in gastos)
@@ -124,11 +91,8 @@ def login():
             session['user'] = 'admin'
             return redirect(url_for('admin'))
         else:
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM usuarios WHERE login = %s AND senha = %s', (login, senha))
-            user = cursor.fetchone()
-            cursor.close()
+            conn = sqlite3.connect('database.db')
+            user = conn.execute('SELECT * FROM usuarios WHERE login = ? AND senha = ?', (login, senha)).fetchone()
             conn.close()
             if user:
                 session['user_id'] = user[0]
@@ -140,19 +104,16 @@ def login():
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
     if 'user' in session and session['user'] == 'admin':
-        conn = get_db_connection()
-        cursor = conn.cursor()
+        conn = sqlite3.connect('database.db')
         if request.method == 'POST':
             nome = request.form['nome']
             idade = request.form['idade']
             data_nascimento = request.form['data_nascimento']
             login = request.form['login']
             senha = request.form['senha']
-            cursor.execute('INSERT INTO usuarios (nome, idade, data_nascimento, login, senha) VALUES (%s, %s, %s, %s, %s)', (nome, idade, data_nascimento, login, senha))
+            conn.execute('INSERT INTO usuarios (nome, idade, data_nascimento, login, senha) VALUES (?, ?, ?, ?, ?)', (nome, idade, data_nascimento, login, senha))
             conn.commit()
-        cursor.execute('SELECT * FROM usuarios')
-        usuarios = cursor.fetchall()
-        cursor.close()
+        usuarios = conn.execute('SELECT * FROM usuarios').fetchall()
         conn.close()
         return render_template('admin.html', usuarios=usuarios)
     else:
@@ -162,14 +123,11 @@ def admin():
 def dashboard():
     if 'user_id' in session:
         user_id = session['user_id']
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM usuarios WHERE id = %s', (user_id,))
-        user = cursor.fetchone()
+        conn = sqlite3.connect('database.db')
+        user = conn.execute('SELECT * FROM usuarios WHERE id = ?', (user_id,)).fetchone()
         
         # Check for an ongoing trip
-        cursor.execute('SELECT * FROM viagens WHERE usuario_id = %s AND finalizada = 0', (user_id,))
-        viagem = cursor.fetchone()
+        viagem = conn.execute('SELECT * FROM viagens WHERE usuario_id = ? AND finalizada = 0', (user_id,)).fetchone()
 
         if viagem:
             if request.method == 'POST':
@@ -177,21 +135,18 @@ def dashboard():
                     motivo = request.form['motivo']
                     observacao = request.form['observacao']
                     valor = request.form['valor']
-                    cursor.execute('INSERT INTO gastos (viagem_id, motivo, observacao, valor) VALUES (%s, %s, %s, %s)', (viagem[0], motivo, observacao, valor))
+                    conn.execute('INSERT INTO gastos (viagem_id, motivo, observacao, valor) VALUES (?, ?, ?, ?)', (viagem[0], motivo, observacao, valor))
                     conn.commit()
                 elif 'finalizar' in request.form:
-                    cursor.execute('UPDATE viagens SET finalizada = 1 WHERE id = %s', (viagem[0],))
+                    conn.execute('UPDATE viagens SET finalizada = 1 WHERE id = ?', (viagem[0],))
                     conn.commit()
                     report_path = generate_report(viagem[0])
                     return redirect(url_for('download_report', viagem_id=viagem[0]))
 
-            cursor.execute('SELECT * FROM gastos WHERE viagem_id = %s', (viagem[0],))
-            gastos = cursor.fetchall()
-            cursor.close()
+            gastos = conn.execute('SELECT * FROM gastos WHERE viagem_id = ?', (viagem[0],)).fetchall()
             conn.close()
             return render_template('dashboard.html', user=user, viagem=viagem, gastos=gastos)
         else:
-            cursor.close()
             conn.close()
             return render_template('dashboard.html', user=user, viagem=None)
     else:
@@ -205,11 +160,9 @@ def inicio_viagem():
             numero_nota = request.form['numero_nota']
             destino = request.form['destino']
             data_inicio = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            cursor.execute('INSERT INTO viagens (usuario_id, numero_nota, destino, data_inicio, finalizada) VALUES (%s, %s, %s, %s, %s)', (user_id, numero_nota, destino, data_inicio, False))
+            conn = sqlite3.connect('database.db')
+            conn.execute('INSERT INTO viagens (usuario_id, numero_nota, destino, data_inicio, finalizada) VALUES (?, ?, ?, ?, ?)', (user_id, numero_nota, destino, data_inicio, False))
             conn.commit()
-            cursor.close()
             conn.close()
             return redirect(url_for('dashboard'))
         return render_template('inicio_viagem.html')
@@ -219,11 +172,9 @@ def inicio_viagem():
 @app.route('/finalizar_viagem/<int:viagem_id>', methods=['POST'])
 def finalizar_viagem(viagem_id):
     if 'user_id' in session:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('UPDATE viagens SET finalizada = 1 WHERE id = %s', (viagem_id,))
+        conn = sqlite3.connect('database.db')
+        conn.execute('UPDATE viagens SET finalizada = 1 WHERE id = ?', (viagem_id,))
         conn.commit()
-        cursor.close()
         conn.close()
         report_path = generate_report(viagem_id)
         return redirect(url_for('download_report', viagem_id=viagem_id))
@@ -246,9 +197,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    try:
-        create_database()
-    except psycopg2.errors.DuplicateDatabase:
-        pass
     init_db()
     app.run(debug=True)
